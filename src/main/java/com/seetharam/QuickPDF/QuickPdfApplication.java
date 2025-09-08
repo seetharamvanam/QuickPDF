bd759a822d8257e6d4d0c5ace698d47010f924b0
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2025-09-07 22:30:04 -0500

    Initial commit

---
+++ src/main/java/com/seetharam/QuickPDF/QuickPdfApplication.java
@@ -0,0 +1,13 @@
+package com.seetharam.QuickPDF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickPdfApplication.class, args);
+	}
+
+}
